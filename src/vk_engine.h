ea7d0a528ceb34234124c7273c1621f5e10a730e
Frank (Yu) Wang <2529230361@qq.com>
2024-11-29 18:46:53 +0800

    Initializing Vulkan

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -5,18 +5,61 @@
 
 #include <vk_types.h>
 
+struct FrameData {
+	VkCommandPool _commandPool;
+	VkCommandBuffer _mainCommandBuffer;
+	VkSemaphore _swapchainSemaphore, _renderSemaphore;
+	VkFence _renderFence;
+};
+
+#ifdef NDEBUG
+constexpr bool bUseValidationLayers = false;
+#else
+constexpr bool bUseValidationLayers = true;
+#endif // NDEBUG
+
+constexpr unsigned int FRAME_OVERLAP = 2;
+
 class VulkanEngine {
-public:
+private:
 
 	bool _isInitialized{ false };
-	int _frameNumber {0};
 	bool stop_rendering{ false };
+	uint32_t _frameNumber{ 0 };
 	VkExtent2D _windowExtent{ 1700 , 900 };
 
 	struct SDL_Window* _window{ nullptr };
 
 	static VulkanEngine& Get();
+	VkInstance _instance;// Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger;// Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU;// GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface;// Vulkan window surface
 
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
+
+	FrameData _frames[FRAME_OVERLAP];
+	VkQueue _graphicsQueue;
+	uint32_t _graphicsQueueFamily;
+
+private:
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
+
+	FrameData& get_current_frame() { return _frames[_frameNumber % FRAME_OVERLAP]; };
+
+public:
 	//initializes everything in the engine
 	void init();
 
