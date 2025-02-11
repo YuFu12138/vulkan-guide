417ba86940b4d45cab81011454cc49505bc29bc4
Frank (Yu) Wang <2529230361@qq.com>
2025-02-11 18:42:04 +0800

    temp

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -60,6 +60,36 @@ struct GPUSceneData {
 	glm::vec4 sunlightColor;
 };
 
+struct GLTFMetallic_Roughness {
+	MaterialPipeline opaquePipeline;
+	MaterialPipeline transparentPipeline;
+
+	VkDescriptorSetLayout materialLayout;
+
+	struct MaterialConstants {
+		glm::vec4 colorFactors;
+		glm::vec4 metal_rough_factors;
+		//padding, we need it anyway for uniform buffers
+		glm::vec4 extra[14];
+	};
+
+	struct MaterialResources {
+		AllocatedImage colorImage;
+		VkSampler colorSampler;
+		AllocatedImage metalRoughImage;
+		VkSampler metalRoughSampler;
+		VkBuffer dataBuffer;
+		uint32_t dataBufferOffset;
+	};
+
+	DescriptorWriter writer;
+
+	void build_pipelines(VulkanEngine* engine);
+	void clear_resources(VkDevice device);
+
+	MaterialInstance write_material(VkDevice device, MaterialPass pass, const MaterialResources& resources, DescriptorAllocatorGrowable& descriptorAllocator);
+};
+
 #ifdef NDEBUG
 constexpr bool bUseValidationLayers = false;
 #else
@@ -69,7 +99,7 @@ constexpr bool bUseValidationLayers = true;
 constexpr unsigned int FRAME_OVERLAP = 2;
 
 class VulkanEngine {
-private:
+public:
 
 	bool _isInitialized{ false };
 	bool stop_rendering{ false };
